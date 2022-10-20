cb5b37fb9940197fba5e752e02560d88c27d3354
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2022-10-20 23:45:33 +0300

    home page, catalog page

--- src/app.js
+++ src/app.js
@@ -1,3 +1,21 @@
-import * as api from './api/data.js';
+import { page, render } from './lib.js'
+import { homePage } from './views/home.js';
+import { catalogPage } from './views/catalog.js';
 
+/*debug*/
+import * as api from './api/data.js';
 window.api = api; 
+
+const root = document.querySelector('main');
+
+page(decorateContex);
+page('/', homePage);
+page('/memes', catalogPage);
+
+page.start();
+
+function decorateContex(ctx, next) {
+ctx.render = (content) => render(content, root)
+    
+    next()
+}
